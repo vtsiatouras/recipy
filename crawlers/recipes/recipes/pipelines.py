52d96f1547b3da006404497adf1e69b63641ee97
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-03-11 00:58:51 +0200

    [feat]: added DBStoragePipeline #WIP

--- crawlers/recipes/recipes/pipelines.py
+++ crawlers/recipes/recipes/pipelines.py
@@ -11,3 +11,27 @@ from itemadapter import ItemAdapter
 class RecipesPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class DBStoragePipeline:
+    # Create DB session here
+    # def __init__(self) -> None:
+    #   Add code here
+
+    # def process_item(self, item, spider):
+    #     """
+    #     Persists recipe item in the database. This method is called for every scraped item.
+    #     :param item: scrapy.Item, required
+    #     :param spider: scrapy.Spider, required
+    #     :return: scrapy.Item
+    #     """
+    #   Add code here
+
+    # def close_spider(self, spider) -> None:
+    #     """
+    #     :param spider: Spider object
+    #     :return: None
+    #     """
+    #    Add code here (close DB session)
+
+    pass
