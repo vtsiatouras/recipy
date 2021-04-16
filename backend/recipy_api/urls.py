99ab8574e972fc9bb8aaf136aaa4a2262d918227
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-04-17 01:24:51 +0300

    [fix]: removed unknown import at urls.py

--- backend/recipy_api/urls.py
+++ backend/recipy_api/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from django.urls import include
 from rest_framework.routers import DefaultRouter
 
-from . import views
-
 
 class OptionalSlashRouter(DefaultRouter):
     """Router to make trailing slashes optional. All URLs work with or without trailing slashes, without redirecting.
