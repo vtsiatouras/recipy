581b74de0c30a62ca3938ff5c6f29915b8d06fc9
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2021-04-17 02:32:28 +0300

    [feat]: added DBStoragePipeline

--- crawlers/recipes/pipelines.py
+++ crawlers/recipes/pipelines.py
@@ -6,32 +6,52 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from django.apps import apps
+from django.db import transaction
 
 
 class RecipesPipeline:
+
     def process_item(self, item, spider):
         return item
 
 
 class DBStoragePipeline:
-    # Create DB session here
-    # def __init__(self) -> None:
-    #   Add code here
 
-    # def process_item(self, item, spider):
-    #     """
-    #     Persists recipe item in the database. This method is called for every scraped item.
-    #     :param item: scrapy.Item, required
-    #     :param spider: scrapy.Spider, required
-    #     :return: scrapy.Item
-    #     """
-    #   Add code here
+    def process_item(self, item, spider):
+        """
+        Persists recipe item in the database. This method is called for every scraped item.
+        :param item: scrapy.Item, required
+        :param spider: scrapy.Spider, required
+        :return: scrapy.Item
+        """
+        site_model = apps.get_model('recipy_api', 'Site')
+        recipe_model = apps.get_model('recipy_api', 'Recipe')
+        ingredient_model = apps.get_model('recipy_api', 'Ingredient')
 
-    # def close_spider(self, spider) -> None:
-    #     """
-    #     :param spider: Spider object
-    #     :return: None
-    #     """
-    #    Add code here (close DB session)
+        # Make sure that all queries will be executed in one single transaction
+        with transaction.atomic():
+            site_obj, _ = site_model.objects.get_or_create(name=spider.name, url=spider.base_url)
 
-    pass
+            # Check if recipe already exists in database
+            try:
+                _ = recipe_model.objects.get(url=item['recipe_url'], name=item['name'], category=item['category'])
+
+                # If no exception is raised ignore the storing procedure
+                return item
+            except recipe_model.DoesNotExist:
+                stored_recipe = recipe_model.objects.create(url=item['recipe_url'],
+                                                            name=item['name'],
+                                                            category=item['category'],
+                                                            instructions=item['instructions'],
+                                                            image_url=item.get('image_url', None),
+                                                            site=site_obj)
+
+                ingredients_ids = []
+                for ingredient in item['ingredients']:
+                    ingredient_obj, _ = ingredient_model.objects.get_or_create(description=ingredient['ingredient'])
+                    ingredients_ids.append(ingredient_obj.id)
+
+                stored_recipe.ingredients.add(*ingredients_ids)
+
+        return item
